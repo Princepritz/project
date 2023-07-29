e234382c523779517515861a3d4dced08b55b6f9
Princepritz <pritamsinghsolanke35@gmail.com>
2023-07-29 22:35:52 +0530

    chnages

--- project.js
+++ project.js
@@ -5,5 +5,13 @@ function  giv(event){
    localStorage.setItem(document.getElementById('date').id.toString(),document.getElementById('date').value.toString());
    localStorage.setItem(document.getElementById('time').id.toString(),document.getElementById('time').value.toString());
 
- 
+   const myobj={
+    name:document.getElementById('name').value,
+    email:document.getElementById('email').value,
+    date:document.getElementById('date').value,
+    time:document.getElementById('time').value
+   }
+   const serialized=JSON.stringify(myobj);
+   localStorage.setItem('myobj',serialized);
+ console.log(localStorage);
 }
