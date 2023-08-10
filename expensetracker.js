a24e28d786f3804734ca16a08fed592ae3009f39
Princepritz <pritamsinghsolanke35@gmail.com>
2023-08-10 22:25:55 +0530

    second

---
+++ expensetracker.js
@@ -0,0 +1,59 @@
+var form=document.getElementById('addForm');
+var itemlist=document.getElementById('items');
+
+
+
+
+form.addEventListener('submit',addItem);
+
+function addItem(e){
+    e.preventDefault();
+
+    var newitem1=document.getElementById('amount').value;
+    var newitem2=document.getElementById('Des').value;
+    var newitem3=document.getElementById('cat').value; 
+    
+    var li=document.createElement('li');
+    li.className='list-group-item';
+    li.appendChild(document.createTextNode(newitem1+"  "+newitem2+"  "+newitem3));
+    
+    var deleteBtn=document.createElement('input');
+    deleteBtn.type='button';
+    deleteBtn.value='Delete Expense';
+    deleteBtn.className='btn btn-danger btn-sm float-right delete';
+
+    var editbtn=document.createElement('input');
+    editbtn.type='button';
+    editbtn.value='Edit Expense';
+    editbtn.className='editbtn';
+    li.appendChild(editbtn);
+
+    li.appendChild(deleteBtn);
+    itemlist.appendChild(li);
+
+    const person={
+        Name:newitem1,
+        Email:newitem2,
+        MobNo:newitem3
+    } 
+    
+    const key=JSON.stringify(newitem2);
+    const value=JSON.stringify(person);
+    localStorage.setItem(key,value);
+
+    deleteBtn.onclick=()=>{
+        var obj=JSON.parse(value);
+        localStorage.removeItem(JSON.stringify(obj.Email));
+        itemlist.removeChild(li);
+    }
+
+    editbtn.onclick=()=>{
+        var obj=JSON.parse(value);
+        document.getElementById('amount').value=obj.Name;
+        document.getElementById('Des').value=obj.Email;
+        localStorage.removeItem(JSON.stringify(obj.Email));
+    }
+    
+
+}
+
